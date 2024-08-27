22587750e1d3ac6a6bca8bc81168b34127990ec6
Martin Blieninger <104385850+cracky22@users.noreply.github.com>
2024-08-27 17:15:26 +0200

    added name to webApp and backend

--- backend/status.php
+++ backend/status.php
@@ -1,18 +1,24 @@
 <?php
-$file = 'status.txt';
+$status_file = 'status.txt';
+$name_file = 'name.txt';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $new_status = $_POST['status'];
-    file_put_contents($file, $new_status);
+    if (isset($_POST['status'])) {
+        $new_status = $_POST['status'];
+        file_put_contents($status_file, $new_status);
+    }
+
+    if (isset($_POST['name'])) {
+        $new_name = $_POST['name'];
+        file_put_contents($name_file, $new_name);
+    }
+
     echo json_encode(['success' => true]);
     exit;
 }
 
-if (file_exists($file)) {
-    $current_status = file_get_contents($file);
-} else {
-    $current_status = 'inside';
-}
+$current_status = file_exists($status_file) ? file_get_contents($status_file) : 'inside';
+$current_name = file_exists($name_file) ? file_get_contents($name_file) : 'Unbenannt';
 
-echo json_encode(['status' => $current_status]);
+echo json_encode(['status' => $current_status, 'name' => $current_name]);
 ?>
